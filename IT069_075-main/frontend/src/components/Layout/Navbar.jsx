f550d1ba794def9edd663ba8c56872b20cc1cb97
Nihal Shah <73733683+nihal-shah@users.noreply.github.com>
2024-04-03 18:07:43 +0530

    Update Navbar.jsx

--- IT069_075-main/frontend/src/components/Layout/Navbar.jsx
+++ IT069_075-main/frontend/src/components/Layout/Navbar.jsx
@@ -13,7 +13,7 @@ const Navbar = () => {
   const handleLogout = async () => {
     try {
       const response = await axios.get(
-        "vercel-jvox.vercel.app/api/v1/user/logout",
+        "https://vercel-jvox.vercel.app/api/v1/user/logout",
         {
           withCredentials: true,
         }
