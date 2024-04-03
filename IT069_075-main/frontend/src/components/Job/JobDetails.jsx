78dd754820664dcf938ccf236b3bc9d7449239e2
Nihal Shah <73733683+nihal-shah@users.noreply.github.com>
2024-04-03 18:05:12 +0530

    Update JobDetails.jsx

--- IT069_075-main/frontend/src/components/Job/JobDetails.jsx
+++ IT069_075-main/frontend/src/components/Job/JobDetails.jsx
@@ -12,7 +12,7 @@ const JobDetails = () => {
 
   useEffect(() => {
     axios
-      .get(`vercel-jvox.vercel.app/api/v1/job/${id}`, {
+      .get(`https://vercel-jvox.vercel.app/api/v1/job/${id}`, {
         withCredentials: true,
       })
       .then((res) => {
